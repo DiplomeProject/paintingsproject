d5c0b0d79445ea9f1ad6894d72ecc9afe8997462
Sanekich <alex.d.muratov@gmail.com>
2025-09-30 19:26:27 +0200

    Pabeda

---
+++ src/components/profilecomponents/Login/Login.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Login.css"; // Подключаем обычный CSS
+
+function Login({ handleLogin, handleInputChange, toggleForm }) {
+    return (
+        <form onSubmit={handleLogin} className="login-form">
+            <h2>DIGITAL BRUSH</h2>
+            <div className="form-group">
+                <label htmlFor="email" className="form-label">Пошта:</label>
+                <input
+                    type="email"
+                    className="form-input"
+                    id="email"
+                    name="email"
+                    onChange={handleInputChange}
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="password" className="form-label">Пароль:</label>
+                <input
+                    type="password"
+                    className="form-input"
+                    id="password"
+                    name="password"
+                    onChange={handleInputChange}
+                />
+            </div>
+            <div className="button-group">
+                <button type="submit" className="btn-next">Увійти</button>
+                <button type="button" onClick={toggleForm} className="btn btn-secondary">
+                    Зареєструватися
+                </button>
+            </div>
+        </form>
+    );
+}
+
+export default Login;
