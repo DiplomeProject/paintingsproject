d3cd16c037e9070fbd62eb843ca817b2f5c16a22
Prykhodchenko Oleksandra <jarolius12@gmail.com>
2025-11-02 16:14:13 +0200

    додала кнопку додати зображення у шоп

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -159,21 +159,29 @@ const Shop = () => {
         <div className={styles.shopPage}>
             <div className={styles.contentWrapper}>
                 <header className={styles.header}>
-                    <h1 className={styles.shopTitle}>Shop</h1>
-                    <div className={styles.searchBarContainer}>
-                        <input
-                            type="text"
-                            className={styles.searchInput}
-                            placeholder="Search by title, artist, style..."
-                            value={searchQuery}
-                            onChange={handleSearchChange}
-                        />
-                        <button className={styles.searchButton}>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
-                                <path d="M21 21l-4.35-4.35M10 18a8 8 0 1 1 0-16 8 8 0 0 1 0 16z" stroke="black" strokeWidth="2" fill="none" />
-                            </svg>
-                        </button>
+                    <div className={styles.headerLeft}>
+                        <h1 className={styles.shopTitle}>SHOP</h1>
+                        <div className={styles.searchBarContainer}>
+                            <input
+                                type="text"
+                                className={styles.searchInput}
+                                placeholder="Title of the art..."
+                                value={searchQuery}
+                                onChange={(e) => setSearchQuery(e.target.value)}
+                            />
+                            <button className={styles.searchButton}>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
+                                    <path d="M21 21l-4.35-4.35M10 18a8 8 0 1 1 0-16 8 8 0 0 1 0 16z" stroke="black" strokeWidth="2" fill="none" />
+                                </svg>
+                            </button>
+                        </div>
                     </div>
+                    <button className={styles.addImageButton}>
+                        <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12.4158 0C5.56994 0 0 5.56994 0 12.4158C0 19.2617 5.56994 24.8317 12.4158 24.8317C19.2617 24.8317 24.8317 19.2617 24.8317 12.4158C24.8317 5.56994 19.2617 0 12.4158 0ZM12.4158 1.91013C18.2293 1.91013 22.9216 6.60236 22.9216 12.4158C22.9216 18.2293 18.2293 22.9216 12.4158 22.9216C6.60236 22.9216 1.91013 18.2293 1.91013 12.4158C1.91013 6.60236 6.60236 1.91013 12.4158 1.91013ZM11.4608 6.68545V11.4608H6.68545V13.3709H11.4608V18.1462H13.3709V13.3709H18.1462V11.4608H13.3709V6.68545H11.4608Z" fill="white"/>
+                        </svg>
+                        ADD IMAGE
+                    </button>
                 </header>
 
                 <CategoryFilters
