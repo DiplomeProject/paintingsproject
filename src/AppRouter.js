a1291e646e7ada2391c3e0ade040092fa7a04e97
Prykhodchenko Oleksandra <jarolius12@gmail.com>
2025-11-03 20:11:52 +0200

    Revert "модалки и артист"
    This reverts commit 379c744362865022072a159e75ade76548983a48.
    
    # Conflicts:
    #	src/components/Commission/Commission.js
    #	src/components/Commission/Commission.module.css
    #	src/components/Shop/Shop.js
    #	src/components/profilecomponents/Login/ForgotPassword/ForgotPassword.js
    #	src/components/profilecomponents/Login/ForgotPassword/ForgotPassword.module.css
    #	src/components/profilecomponents/Profile/Profile.js
    #	src/components/profilecomponents/Profile/Profile.module.css
    #	src/components/profilecomponents/Profile/UserProfile/DigitalBrushProfile.js
    #	src/components/profilecomponents/Profile/UserProfile/DigitalBrushProfile.module.css
    #	src/components/profilecomponents/Register/Register.js

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -4,33 +4,18 @@ import Profile from "./components/profilecomponents/Profile/Profile";
 import HomePage from "./components/HomePage/HomePage";
 import Shop from "./components/Shop/Shop";
 import Register from "./components/profilecomponents/Register/Register";
-import Artists from "./components/Artists/Artists";
+import Commission from "./components/Commission/Commission";
 
 export default function AppRouter() {
-    return ( <
-        Routes >
-        <
-        Route path = '/'
-        element = { < HomePage / > }
-        /> <
-        Route path = 'homepage'
-        element = { < HomePage / > }
-        /> <
-        Route path = 'profile'
-        element = { < Profile / > }
-        /> { /* Додаємо маршрут для реєстрації */ } <
-        Route path = 'register'
-        element = { < Register / > }
-        /> <
-        Route path = 'shop'
-        element = { < Shop / > }
-        /> <
-        Route path = 'artists'
-        element = { < Artists / > }
-        /> { /* Поки що веде на HomePage */ } <
-        Route path = 'commission'
-        element = { < HomePage / > }
-        /> <
-        /Routes>
+    return (
+        <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='homepage' element={<HomePage />} />
+            <Route path='profile' element={<Profile />} />
+            <Route path='register' element={<Register />} /> {/* Додаємо маршрут для реєстрації */}
+            <Route path='shop' element={<Shop />} />
+            <Route path='artists' element={<HomePage />} /> {/* Поки що веде на HomePage */}
+            <Route path='commission' element={<Commission />} />
+        </Routes>
     )
 }
