8dded7d8409fa70c4e6804c0fb6bd423341033f2
Sanekich <alex.d.muratov@gmail.com>
2025-12-03 19:33:33 +0100

    one more test

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ db.getConnection()
     .then(conn => {
         console.log('✅ Database connected');
         conn.release();
-        app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+        app.listen(PORT, '0.0.0.0', () => {
+            console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
+        });
     })
     .catch(err => console.error('❌ DB connection failed:', err));
