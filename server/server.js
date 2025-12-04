761500756b8dd70e6683a1aa8a3c02c320dc6261
Sanekich <alex.d.muratov@gmail.com>
2025-12-04 22:14:56 +0100

    вернул как было

--- server/server.js
+++ server/server.js
@@ -8,9 +8,8 @@ db.getConnection()
     .then(conn => {
         console.log('✅ Database connected');
         conn.release();
-        app.listen(PORT, '0.0.0.0', () => {
+        app.listen(PORT, '172.17.3.23', () => {
             console.log(`🚀 Server running on http://172.17.3.23:${PORT}`);
         });
-
     })
     .catch(err => console.error('❌ DB connection failed:', err));
