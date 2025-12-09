54e401582f7494bcbda90e178a5a1f4206665cb8
Sanekich <alex.d.muratov@gmail.com>
2025-12-09 18:01:07 +0100

    test??

--- server/app.js
+++ server/app.js
@@ -12,27 +12,48 @@ const artists = require('./Artists/Artists');
 const app = express();
 
 app.use(express.json());
+
 app.use(cors({
-    origin: [
-        'http://172.17.3.24:8080',
-        'http://localhost:3000'
-    ],
+    origin: (origin, callback) => {
+        const allowed = [
+            'http://172.17.3.24:8080',
+            'http://localhost:3000'
+        ];
+        if (!origin || allowed.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true
 }));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+
+    next();
+});
+
 app.use(session({
-    secret: require('crypto').randomBytes(64).toString('hex'),
+    secret: '3c1f9d8e7a4b2c0df5a98c4b7e1d263f8b4a1dce92f07bd38a6f2c51d47e9032',
     resave: false,
     saveUninitialized: false,
     cookie: { secure: false, maxAge: 86400000 }
 }));
 
-app.use(authRoutes);
-app.use(paintingRoutes);
-app.use(profileRoutes);
-app.use(searchRoutes);
-app.use(commissions);
-app.use(artists);
-
+// Prefix all routes for consistency
+app.use('/api', authRoutes);
+app.use('/api', paintingRoutes);
+app.use('/api', profileRoutes);
+app.use('/api', searchRoutes);
+app.use('/api', commissions);
+app.use('/api', artists);
 
 module.exports = app;
