8a03ffcaa2383705ddf8ec311aaf47b7ea21aff4
Sanekich <alex.d.muratov@gmail.com>
2025-12-11 21:09:58 +0100

    test

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,7 +2,7 @@ module.exports = (req, res, next) => {
     // We can remove the OPTIONS check because app.js handles preflight globally now.
     
     if (!req.session.user) {
-        return res.status(401).json({ success: false, message: 'Not authorized' });
+       next();
     }
     next();
 };
