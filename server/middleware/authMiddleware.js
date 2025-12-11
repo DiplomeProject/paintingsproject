1c4de7bfea10831e830f593473b934fd769a1e28
Sanekich <alex.d.muratov@gmail.com>
2025-12-11 16:57:22 +0100

    fix

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,4 +1,9 @@
 module.exports = (req, res, next) => {
+    // allow preflight
+    if (req.method === 'OPTIONS') {
+        return next();
+    }
+
     if (!req.session.user) {
         return res.status(401).json({ success: false, message: 'Not authorized' });
     }
